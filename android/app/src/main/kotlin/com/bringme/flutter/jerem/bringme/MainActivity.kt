b89594e6ddb8244ffdc0daf1cd2895ab6a285863
jeremys <jeremysurget77@gmail.com>
2019-11-08 14:29:22 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/bringme/flutter/jerem/bringme/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.bringme.flutter.jerem.bringme
+
+import android.os.Bundle
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
