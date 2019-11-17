ccb94e825da432bc784e33c6e5ef474e71574008
jeremys <jeremysurget77@gmail.com>
2019-11-17 22:25:08 +0100

    added some information to the dialog

--- lib/user/propositionFromDemand.dart
+++ lib/user/propositionFromDemand.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:bringme/services/crud.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:bringme/root_page.dart';
 
 class PropositionFromDemand extends StatefulWidget {
   PropositionFromDemand(
@@ -30,16 +31,18 @@ class _PropositionFromDemandState extends State<PropositionFromDemand> {
   bool _isLoading = false;
   bool _loadingData = false;
 
-
   @override
-  void initState(){
+  void initState() {
     super.initState();
 
     setState(() {
       _loadingData = true;
     });
-    for(int i = 0; i < widget.listProposition.length; i++){
-      crudObj.getDataFromDeliveryManFromDocumentWithID(widget.listProposition[i]['deliveryManId']).then((value){
+    for (int i = 0; i < widget.listProposition.length; i++) {
+      crudObj
+          .getDataFromDeliveryManFromDocumentWithID(
+              widget.listProposition[i]['deliveryManId'])
+          .then((value) {
         Map<String, dynamic> map = value.data;
         map['deliveryManId'] = widget.listProposition[i]['deliveryManId'];
         map['price'] = widget.listProposition[i]['price'];
@@ -51,9 +54,7 @@ class _PropositionFromDemandState extends State<PropositionFromDemand> {
     setState(() {
       _loadingData = false;
     });
-
   }
-
 
   _acceptProposition(deliveryManId) async {
     setState(() {
@@ -129,41 +130,38 @@ class _PropositionFromDemandState extends State<PropositionFromDemand> {
         ));
   }
 
-
-  void _showDialog(deliveryManId, name, price, phone){
+  void _showDialog(deliveryManId, name, price, phone) {
     showDialog(
-      context: context,
-      builder: (BuildContext context){
-        return AlertDialog(
-          title: Text("Information livreur"),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Text("Prénom : " + name),
-              Text("Téléphone : " + phone),
-              Text("Prix proposé : " + price),
-              Text("Heure de livraison : - à ajouter -"),
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("Information livreur"),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text("Prénom : " + name),
+                Text("Téléphone : " + phone),
+                Text("Prix proposé : " + price + "€"),
+                Text("Heure de livraison : - à ajouter -"),
+              ],
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("fermer"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              FlatButton(
+                child: Text("Accepter"),
+                onPressed: () {
+                  _acceptProposition(deliveryManId);
+                  Navigator.pushReplacementNamed(context, "/");
+                },
+              ),
             ],
-          ),
-          actions: <Widget>[
-            FlatButton(
-              child: Text("fermer"),
-              onPressed: (){
-                Navigator.of(context).pop();
-              },
-            ),
-
-            FlatButton(
-              child: Text("Accepter"),
-              onPressed: (){
-                _acceptProposition(deliveryManId);
-
-              },
-            ),
-          ],
-        );
-      }
-    );
+          );
+        });
   }
 
   _buildListOfProposition() {
@@ -172,9 +170,9 @@ class _PropositionFromDemandState extends State<PropositionFromDemand> {
     //sur la meme page parce que sinon beaucoup trop de page dans des pages
     //c'est lourd et chiant à naviguer
 
-    if(_loadingData || deliveryManData.isEmpty){
+    if (_loadingData || deliveryManData.isEmpty) {
       return CircularProgressIndicator();
-    }else {
+    } else {
       return ListView.builder(
         itemCount: widget.listProposition.length,
         itemBuilder: (context, index) {
@@ -184,11 +182,15 @@ class _PropositionFromDemandState extends State<PropositionFromDemand> {
           return Container(
             child: ListTile(
               title: Text(deliveryManName),
-              subtitle: Text(price),
+              subtitle: Text(price + "€"),
               trailing: FlatButton(
-                child: Icon(Icons.check_circle_outline),
+                child: Text(
+                  "Voir",
+                  style: TextStyle(fontWeight: FontWeight.w600),
+                ),
                 onPressed: () {
-                  _showDialog(deliveryManData[index]['deliveryManId'], deliveryManName, price, phone);
+                  _showDialog(deliveryManData[index]['deliveryManId'],
+                      deliveryManName, price, phone);
                 },
               ),
             ),
