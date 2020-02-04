abce8c252dd993b75a9d099fd680df3a5eccb780
jeremys <jeremysurget77@gmail.com>
2020-02-04 17:14:33 +0100

    notification payload now show information about the delivery request

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+var moment = require('moment');
 admin.initializeApp();
 
 
@@ -13,12 +14,15 @@ exports.notifNewDelivery = functions.firestore.document('request/{requestId}').o
     //console.log(newRequest);
     //console.log(t);
 
-    let date_ob = new Date(newRequest.deliveryDate);
-    let date = date_ob.getDate();
-    console.log(date);
+    console.log(moment(newRequest.deliveryDate));
+
+    //trop chiant d'avoir l'heure avec un unix timestamp et moment js
+    //let hour = moment(newRequest.deliveryDate).format("hh:mm");
+    let date = moment(newRequest.deliveryDate).format("DD/MM/YY");
+
 
     const payload = {
-        notification: { title: 'Nouvelle demande de livraison !', body: "coucou" + ' à ' + newRequest.destination, badge: '1', sound: 'default', }
+        notification: { title: 'Nouvelle demande de livraison !', body: 'Le ' + date + ' à destination de ' + newRequest.destination, badge: '1', sound: 'default', }
     };
 
     //permet de stocker les tokens de notif de tout les delivery man pour pouvoir envoyer les notifs a tout ceux qui en ont un
